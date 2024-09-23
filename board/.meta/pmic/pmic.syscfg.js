1d1e1a82c2d2f23dec9706d1b39502671a199ece
Aswathi R <a0505177@ti.com>
2024-09-24 00:33:12 +0530

    am261x: pmic: disable pmic wdg reset
    - add board_open_close templates for am261x
    - add APIs to modify the WD_ENABLE_REG of pmic using i2c
    - update templates list based on soc in pmic.syscfg.js
    
    Fixes: MCUSDK-13707
    Signed-off-by: Aswathi R <a-r1@ti.com>

--- board/.meta/pmic/pmic.syscfg.js
+++ board/.meta/pmic/pmic.syscfg.js
@@ -28,27 +28,58 @@ function getConfigurables() {
 
 let pmic_module_name = "/board/pmic/pmic";
 
+function get_templates() {
+    let templates = {}
+
+    if(common.getSocName() == "am261x")
+    {
+        templates = {
+            "/board/board/board_open_close.c.xdt": {
+                board_open_close_config: "/board/pmic/templates/pmic_open_close_config_v1.c.xdt",
+                board_close: "/board/pmic/templates/pmic_close.c.xdt",
+            },
+            "/board/board/board_open_close.h.xdt": {
+                board_open_close_config: "/board/pmic/templates/pmic_open_close_v1.h.xdt",
+            },
+            "/board/board/board_config.h.xdt": {
+                board_config: "/board/pmic/templates/pmic.h.xdt",
+            },
+            "/drivers/system/drivers_open_close.c.xdt": {
+                driver_open: "/board/pmic/templates/pmic_open.c.xdt",
+            },
+            "/drivers/system/drivers_open_close.h.xdt": {
+                driver_open_close_config: "/board/pmic/templates/pmic_driver_open_close.h.xdt",
+            },
+        }
+    }
+    else {
+        templates = {
+            "/board/board/board_open_close.c.xdt": {
+                board_open_close_config: "/board/pmic/templates/pmic_open_close_config.c.xdt",
+                board_close: "/board/pmic/templates/pmic_close.c.xdt",
+            },
+            "/board/board/board_open_close.h.xdt": {
+                board_open_close_config: "/board/pmic/templates/pmic_open_close.h.xdt",
+            },
+            "/board/board/board_config.h.xdt": {
+                board_config: "/board/pmic/templates/pmic.h.xdt",
+            },
+            "/drivers/system/drivers_open_close.c.xdt": {
+                driver_open: "/board/pmic/templates/pmic_open.c.xdt",
+            },
+            "/drivers/system/drivers_open_close.h.xdt": {
+                driver_open_close_config: "/board/pmic/templates/pmic_driver_open_close.h.xdt",
+            },
+        }
+    }
+
+    return templates;
+}
+
 let pmic_module = {
     displayName: "PMIC",
 
-    templates: {
-        "/board/board/board_open_close.c.xdt": {
-            board_open_close_config: "/board/pmic/templates/pmic_open_close_config.c.xdt",
-            board_close: "/board/pmic/templates/pmic_close.c.xdt",
-        },
-        "/board/board/board_open_close.h.xdt": {
-            board_open_close_config: "/board/pmic/templates/pmic_open_close.h.xdt",
-        },
-        "/board/board/board_config.h.xdt": {
-            board_config: "/board/pmic/templates/pmic.h.xdt",
-        },
-        "/drivers/system/drivers_open_close.c.xdt": {
-            driver_open: "/board/pmic/templates/pmic_open.c.xdt",
-        },
-        "/drivers/system/drivers_open_close.h.xdt": {
-            driver_open_close_config: "/board/pmic/templates/pmic_driver_open_close.h.xdt",
-        },
-    },
+    templates: get_templates(),
     defaultInstanceName: "CONFIG_PMIC",
     config: getConfigurables(),
     moduleStatic: {
@@ -74,6 +105,13 @@ function moduleInstances(instance) {
             name: "peripheralDriver",
             displayName: "MCSPI Configuration",
             moduleName: '/drivers/mcspi/mcspi',
+        });
+    }
+    if(config.type == "I2C") {
+        modInstances.push({
+            name: "peripheralDriver",
+            displayName: "I2C Configuration",
+            moduleName: '/drivers/i2c/i2c',
         });
     }
 
--- board/.meta/pmic/pmic_am261x.syscfg.js
+++ board/.meta/pmic/pmic_am261x.syscfg.js
@@ -3,12 +3,12 @@ let common = system.getScript("/common");
 
 let pmic_devices = [
     {
-        name        : "TPS653860xx",
-        type        : "MCSPI",
-        instance    : "SPI1",
-        deviceType  : "PMIC_DEV_BB_TPS65386X",
+        name        : "TPS65036xx",
+        type        : "I2C",
+        instance    : "I2C0",
+        deviceType  : "PMIC_DEV_BB_TPS65036X",
         instType    : "PMIC_MAIN_INST",
-        commMode    : "PMIC_INTF_SPI",
+        commMode    : "PMIC_INTF_I2C",
     },
 ];
 
