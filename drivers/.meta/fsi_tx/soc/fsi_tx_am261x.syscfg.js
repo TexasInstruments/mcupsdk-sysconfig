118d5c30589b2c12a299453233fb9f19d1b37109
Aswathi R <a0505177@ti.com>
2024-10-24 18:52:08 +0530

    am261x: fsi: fix clock
    - change fsi_tx clock freq based on r5 clock freq
    
    Fixes: MCUSDK-13810
    Signed-off-by: Aswathi R <a-r1@ti.com>

--- drivers/.meta/fsi_tx/soc/fsi_tx_am261x.syscfg.js
+++ drivers/.meta/fsi_tx/soc/fsi_tx_am261x.syscfg.js
@@ -1,62 +1,71 @@
 
 let common = system.getScript("/common");
 
-let fsi_tx_func_clk = 500 * 1000 * 1000;
-
 const staticConfig = [
     {
         name: "FSITX0",
         baseAddr: "CSL_CONTROLSS_FSI_TX0_U_BASE",
-        funcClk: fsi_tx_func_clk,
+        funcClk: getDefaultClkRate(),
         clockIds        : [ "SOC_RcmPeripheralId_CONTROLSS_PLL" ],
         clockFrequencies: [
             {
                 moduleId: "SOC_RcmPeripheralId_CONTROLSS_PLL",
                 clkId   : "SOC_RcmPeripheralClockSource_DPLL_CORE_HSDIV0_CLKOUT2",
-                clkRate : fsi_tx_func_clk,
+                clkRate : getDefaultClkRate(),
             },
         ],
     },
     {
         name: "FSITX1",
         baseAddr: "CSL_CONTROLSS_FSI_TX1_U_BASE",
-        funcClk: fsi_tx_func_clk,
+        funcClk: getDefaultClkRate(),
         clockIds        : [ "SOC_RcmPeripheralId_CONTROLSS_PLL" ],
         clockFrequencies: [
             {
                 moduleId: "SOC_RcmPeripheralId_CONTROLSS_PLL",
                 clkId   : "SOC_RcmPeripheralClockSource_DPLL_CORE_HSDIV0_CLKOUT2",
-                clkRate : fsi_tx_func_clk,
+                clkRate : getDefaultClkRate(),
             },
         ],
     },
     {
         name: "FSITX2",
         baseAddr: "CSL_CONTROLSS_FSI_TX2_U_BASE",
-        funcClk: fsi_tx_func_clk,
+        funcClk: getDefaultClkRate(),
         clockIds        : [ "SOC_RcmPeripheralId_CONTROLSS_PLL" ],
         clockFrequencies: [
             {
                 moduleId: "SOC_RcmPeripheralId_CONTROLSS_PLL",
                 clkId   : "SOC_RcmPeripheralClockSource_DPLL_CORE_HSDIV0_CLKOUT2",
-                clkRate : fsi_tx_func_clk,
+                clkRate : getDefaultClkRate(),
             },
         ],
     },
     {
         name: "FSITX3",
         baseAddr: "CSL_CONTROLSS_FSI_TX3_U_BASE",
-        funcClk: fsi_tx_func_clk,
+        funcClk: getDefaultClkRate(),
         clockIds        : [ "SOC_RcmPeripheralId_CONTROLSS_PLL" ],
         clockFrequencies: [
             {
                 moduleId: "SOC_RcmPeripheralId_CONTROLSS_PLL",
                 clkId   : "SOC_RcmPeripheralClockSource_DPLL_CORE_HSDIV0_CLKOUT2",
-                clkRate : fsi_tx_func_clk,
+                clkRate : getDefaultClkRate(),
             },
         ],
     },
 ];
+
+function getDefaultClkRate() {
+    let fsi_tx_func_clk = 400 * 1000 * 1000;
+
+    if(common.getR5Freq() == "500MHz")
+    {
+        fsi_tx_func_clk = 500 * 1000 * 1000;
+    }
+
+    return fsi_tx_func_clk;
+}
 
 function getStaticConfigArr() {
     return staticConfig;
@@ -69,6 +78,7 @@ function getInterfaceName(inst) {
 let soc = {
     getStaticConfigArr,
     getInterfaceName,
+    getDefaultClkRate,
     interruptXbarConfig: true,
 };
 
