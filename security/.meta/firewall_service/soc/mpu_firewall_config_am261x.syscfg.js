86fb402b9c432ef4dc6aa9b17d798f2445238484
Arpit Sanghai <a-sanghai@ti.com>
2024-11-12 13:37:37 +0530

    am261x: firewall: update firewall config
     -Update the sysconfig for am261x firewall
    
    Fixes: TIFSMCU-2333
    
    Signed-off-by: Arpit Sanghai <a-sanghai@ti.com>

--- security/.meta/firewall_service/soc/mpu_firewall_config_am261x.syscfg.js
+++ security/.meta/firewall_service/soc/mpu_firewall_config_am261x.syscfg.js
@@ -21,68 +21,26 @@ const mpu_firewall_config = [
         ]
     },
     {
-        name: "L2OCRAM_BANK3_SLV",
-        regionCount: 8,
-        memSpace : [
-            { startAddr: 0x70180000, size: 0x80000 },
-        ]
-    },
-    {
-        name: "L2OCRAM_BANK4_SLV",
-        regionCount: 8,
-        memSpace : [
-            { startAddr: 0x70200000, size: 0x80000 },
-        ]
-    },
-    {
-        name: "L2OCRAM_BANK5_SLV",
-        regionCount: 8,
-        memSpace : [
-            { startAddr: 0x70280000, size: 0x80000 },
-        ]
-    },
-    {
         name: "R5SS0_CORE0_AXIS_SLV",
         regionCount: 8,
         memSpace : [
-            { startAddr: 0x78000000, size: 0x10000 },
-            { startAddr: 0x78060000, size: 0x400 },
-            { startAddr: 0x78100000, size: 0x10000 },
+            { startAddr: 0x78000000, size: 0x20000 },
+            { startAddr: 0x78100000, size: 0x20000 },
             { startAddr: 0x74000000, size: 0x800000 },
             { startAddr: 0x74800000, size: 0x800000 },
+            { startAddr: 0x78060000, size: 0x400 },            
+            
         ]
     },
     {
         name: "R5SS0_CORE1_AXIS_SLV",
         regionCount: 8,
         memSpace : [
-            { startAddr: 0x78200000, size: 0x8000 },
-            { startAddr: 0x78260000, size: 0x400 },
-            { startAddr: 0x78300000, size: 0x8000 },
+            { startAddr: 0x78200000, size: 0x20000 },
+            { startAddr: 0x78300000, size: 0x20000 },
             { startAddr: 0x75000000, size: 0x800000 },
             { startAddr: 0x75800000, size: 0x800000 },
-        ]
-    },
-    {
-        name: "R5SS1_CORE0_AXIS_SLV",
-        regionCount: 8,
-        memSpace : [
-            { startAddr: 0x78400000, size: 0x10000 },
-            { startAddr: 0x78460000, size: 0x400 },
-            { startAddr: 0x78500000, size: 0x10000 },
-            { startAddr: 0x76000000, size: 0x800000 },
-            { startAddr: 0x76800000, size: 0x800000 },
-        ]
-    },
-    {
-        name: "R5SS1_CORE1_AXIS_SLV",
-        regionCount: 8,
-        memSpace : [
-            { startAddr: 0x78600000, size: 0x8000 },
-            { startAddr: 0x78660000, size: 0x400 },
-            { startAddr: 0x78700000, size: 0x8000 },
-            { startAddr: 0x77000000, size: 0x800000 },
-            { startAddr: 0x77800000, size: 0x800000 },
+            { startAddr: 0x78260000, size: 0x400},
         ]
     },
     {
@@ -102,10 +60,36 @@ const mpu_firewall_config = [
         ]
     },
     {
+        name: "OSPI1_SLV",
+        regionCount: 8,
+        memSpace : [
+            { startAddr: 0xA0000000, size: 0x8000000 },
+        ]
+    },
+    {
         name: "OSPI0_CFG_SLV",
         regionCount: 4,
         memSpace : [
-            { startAddr: 0x53800000, size: 0x10000 },
+            { startAddr: 0x53800000, size: 0x1000 },
+            { startAddr: 0x53801000, size: 0x1000 },
+            { startAddr: 0x53802000, size: 0x1000 },
+            { startAddr: 0x53806000, size: 0x1000 },
+            { startAddr: 0x53807000, size: 0x1000 },
+            { startAddr: 0x53808000, size: 0x1000 },
+            { startAddr: 0x5380B000, size: 0x1000 },
+            { startAddr: 0x5380C000, size: 0x1000 },
+            { startAddr: 0x5380D000, size: 0x1000 },
+            { startAddr: 0x5380E000, size: 0x1000 },
+            { startAddr: 0x5380F000, size: 0x1000 },
+        ]
+    },
+    {
+        name: "OSPI1_CFG_SLV",
+        regionCount: 4,
+        memSpace : [
+            { startAddr: 0x53A00000, size: 0x1000 },
+            { startAddr: 0x53A01000, size: 0x1000 },
+            { startAddr: 0x53A02000, size: 0x1000 },
         ]
     },
     {
@@ -137,31 +121,17 @@ const mpu_firewall_config = [
         ]
     },
     {
-        name: "R5SS1_CORE0_AHB_MST",
-        regionCount: 16,
-        memSpace : [
-            { startAddr: 0x50000000, size: 0x10000000 },
-        ]
-    },
-    {
-        name: "R5SS1_CORE1_AHB_MST",
-        regionCount: 16,
-        memSpace : [
-            { startAddr: 0x50000000, size: 0x10000000 },
-        ]
-    },
-    {
         name: "R5SS0_SLV",
-        regionCount: 4,
+        regionCount: 8,
         memSpace : [
-            { startAddr: 0x53000000, size: 0x500400 },
-        ]
-    },
-    {
-        name: "R5SS1_SLV",
-        regionCount: 4,
-        memSpace : [
-            { startAddr: 0x53004000, size: 0x50C400 },
+            { startAddr: 0x53000000, size: 0x210 },
+            { startAddr: 0x53003000, size: 0x210 },
+            { startAddr: 0x53210000, size: 0x3C },
+            { startAddr: 0x53500000, size: 0xAC },
+            { startAddr: 0x53020000, size: 0x2000 },
+            { startAddr: 0x53024000, size: 0x2000 },
+            { startAddr: 0x53212000, size: 0x400 },
+            { startAddr: 0x53213000, size: 0x400 },
         ]
     },
 ];
@@ -170,14 +140,14 @@ const mpu_firewall_config = [
 const id_list = [
 	{ name: "R5FSS0_0", displayName:"R5FSS0_0" },
 	{ name: "R5FSS0_1", displayName:"R5FSS0_1" },
-    { name: "R5FSS1_0", displayName:"R5FSS1_0" },
-	{ name: "R5FSS1_1", displayName:"R5FSS1_1" },
-    { name: "ICSSM", displayName:"ICSSM" },
+    { name: "ICSSM0", displayName:"ICSSM0" },
+    { name: "ICSSM1", displayName:"ICSSM1" },
 	{ name: "CPSW", displayName:"CPSW" },
+    { name: "USB", displayName:"USB" },
     { name: "AIDX", displayName:"EXTERNAL ID" },
 ]
 
-const default_id_list = ["R5FSS0_0", "R5FSS0_1", "R5FSS1_0", "R5FSS1_1"]
+const default_id_list = ["R5FSS0_0", "R5FSS0_1"]
 
 function getConfigArr() {
     return mpu_firewall_config;
