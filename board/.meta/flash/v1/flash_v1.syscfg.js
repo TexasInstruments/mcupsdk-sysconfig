dc956224f908d8de5197351127a69d84ef5a24c9
Piyush Sharma <p-sharma3@ti.com>
2025-02-16 15:35:39 +0530

    am64x/am243x: JSON: Fix load Json flash config file
    - Load from JSON button was failing to load the JSON
    flash config file. Updated new path to copyutil.js
    
    Fixes: SITSW-5420
    
    Signed-off-by: Piyush Sharma <p-sharma3@ti.com>

--- board/.meta/flash/v1/flash_v1.syscfg.js
+++ board/.meta/flash/v1/flash_v1.syscfg.js
@@ -485,10 +485,10 @@ let flash_module = {
                         let copyScriptPath = ""
                         if(system.getOS() == "win") {
                             sdkPath = products[0].path.split("\\.metadata\\product.json")[0];
-                            copyScriptPath = sdkPath + "//source//board//.meta//flash//copyutil.js";
+                            copyScriptPath = sdkPath + "//source//sysconfig//board//.meta//flash//copyutil.js";
                         } else {
                             sdkPath = products[0].path.split("/.metadata/product.json")[0];
-                            copyScriptPath = sdkPath + "/source/board/.meta/flash/copyutil.js";
+                            copyScriptPath = sdkPath + "/source/sysconfig/board/.meta/flash/copyutil.js";
                         }
                         return {
                             command: nodeCmd,
