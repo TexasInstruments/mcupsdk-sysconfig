7f527bd44ebf77b288547471485e8e0b8288b9e4
Anil Dali <anil.dali@ti.com>
2025-04-15 21:00:50 +0530

    am263px: rti : Update rti clk src
    - Updated RTI clock select mux address
    for R0-R3 and for R4-R7 separately
    
    Fixes: MCUSDK-14371
    
    Signed-off-by: Anil Dali <anil.dali@ti.com>

--- drivers/.meta/rti/soc/rti_am263px.syscfg.js
+++ drivers/.meta/rti/soc/rti_am263px.syscfg.js
@@ -72,7 +72,7 @@ function getStaticConfigArr() {
                 baseAddr: 0x52180000 + i*0x1000,
                 /* RTI Interrupts are not continous. RTI 0-3 are grouped together and RTI 4-7 are grouped separately */
                 hwiIntNum: ((i < 4) ? (84 + (7 * i)) : (219 + (6 * (i - 4)))),
-                clkSelMuxAddr: 0x53208000 + 0x114 + 4*i,
+                clkSelMuxAddr: ((i < 4) ? (0x53208000 + 0x114 + (4*i)) : (0x53208000 + 0x19C + (4 * (i - 4)))),
                 disableClkSourceConfig: false,
                 lockUnlockDomain: "SOC_DOMAIN_ID_MAIN",
                 lockUnlockPartition: "MSS_RCM_PARTITION0",
