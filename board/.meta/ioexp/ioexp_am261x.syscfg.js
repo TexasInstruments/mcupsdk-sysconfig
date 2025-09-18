b56318c6aa3035b29084737d82b10840fffbaeb6
Shivank Sharma <s-sharma6@ti.com>
2025-09-18 12:32:32 +0530

    am261x: ioexp: update ioexp pinout for LP-RevA
     - update TCA6408ARGTR pinout for LP-RevA
    
    Fixes: MCUSDK-14954
    
    Signed-off-by: Shivank Sharma <s-sharma6@ti.com>

--- board/.meta/ioexp/ioexp_am261x.syscfg.js
+++ board/.meta/ioexp/ioexp_am261x.syscfg.js
@@ -34,6 +34,7 @@ let common = system.getScript("/common");
  *    - Used in both display names and generated macro definitions
  */
 let ioexp_devices = [
+    /*E1, E2 and RevA have same configuration */
     {
         name        : "TCA6408ARGTR",
         type        : "I2C",
@@ -49,9 +50,10 @@ let ioexp_devices = [
             {portNumber : 0, pinNumber : 7, pinName : "BP_MUX_SW_SO"},
         ],
         i2cAddress  : 0x20,
-        board       : "LP-E1",
+        board       : "LP-E2",
         driverName  : "ioexp_tca6408"
     },
+    /*E1 and E2 have same configuration */
     {
         name        : "TCA6408ARGTR",
         type        : "I2C",
@@ -67,11 +69,48 @@ let ioexp_devices = [
             {portNumber : 0, pinNumber : 7, pinName : "OSPI1_MUX_SEL"},
         ],
         i2cAddress  : 0x21,
-        board       : "LP-E1",
+        board       : "LP-E2",
+        driverName  : "ioexp_tca6408"
+    },
+    /*RevA configuration */
+    {
+        name        : "TCA6408ARGT",
+        type        : "I2C",
+        instance    : "I2C0",
+        pinSet      : [
+            {portNumber : 0, pinNumber : 0, pinName : "USB2.0_MUX_SEL0"},
+            {portNumber : 0, pinNumber : 1, pinName : "VPP_LDO_EN"},
+            {portNumber : 0, pinNumber : 2, pinName : "LED_DRIVER_EN"},
+            {portNumber : 0, pinNumber : 3, pinName : "MCAN_MUX_SEL"},
+            {portNumber : 0, pinNumber : 4, pinName : "BP_BO_MUX_EN"},
+            {portNumber : 0, pinNumber : 5, pinName : "BP_MUX_SW_S1"},
+            {portNumber : 0, pinNumber : 6, pinName : "BP_BO_MUX_EN_N"},
+            {portNumber : 0, pinNumber : 7, pinName : "BP_MUX_SW_SO"},
+        ],
+        i2cAddress  : 0x20,
+        board       : "LP-RevA",
         driverName  : "ioexp_tca6408"
     },
     {
-        name        : "TCA6408ARGTR",
+        name        : "TCA6408ARGT",
+        type        : "I2C",
+        instance    : "I2C0",
+        pinSet      : [
+            {portNumber : 0, pinNumber : 0, pinName : "BP_MUX_SW_S6"},
+            {portNumber : 0, pinNumber : 1, pinName : "ETH0_CPSW2_RST"},
+            {portNumber : 0, pinNumber : 2, pinName : "ETH1_CPSW1_RST"},
+            {portNumber : 0, pinNumber : 3, pinName : "MDIO/MDC_MUX_SEL"},
+            {portNumber : 0, pinNumber : 4, pinName : "BP_MUX_SW_S4"},
+            {portNumber : 0, pinNumber : 5, pinName : "BP_MUX_SW_S5"},
+            {portNumber : 0, pinNumber : 6, pinName : "FSI_EQEP_MUX_SEL"},
+            {portNumber : 0, pinNumber : 7, pinName : "OSPI1_MUX_SEL"},
+        ],
+        i2cAddress  : 0x21,
+        board       : "LP-RevA",
+        driverName  : "ioexp_tca6408"
+    },
+    {
+        name        : "TCA6408ARGT",
         type        : "I2C",
         instance    : "I2C0",
         pinSet      : [
@@ -85,7 +124,7 @@ let ioexp_devices = [
             {portNumber : 0, pinNumber : 7, pinName : "IOEXP_OUT_P7"},
         ],
         i2cAddress  : 0x21,
-        board       : "SOM-E1",
+        board       : "SOM-RevA",
         driverName  : "ioexp_tca6408"
     },
 ];
